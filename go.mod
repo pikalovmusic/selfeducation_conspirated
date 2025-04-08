fe66258aa7685bb0ecc31fb6434def9bb7f095d7
pikalovmusic <166620147+pikalovmusic@users.noreply.github.com>
2025-04-08 13:22:27 +0500

    clone repo & task 1.5.5

---
+++ go.mod
@@ -0,0 +1,8 @@
+module selfeducation_conspirated
+
+go 1.19
+
+require (
+	github.com/ekomobile/dadata/v2 v2.16.0
+	github.com/go-chi/chi v1.5.5
+)
---
+++ proxy/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	proxy := NewReverseProxy("hugo", "1313")
+	r.Use(proxy.ReverseProxy)
+	r.HandleFunc("/api/*", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello from API"))
+		log.Println("API request")
+	})
+
+	fmt.Println("Server started at :8080...")
+	http.ListenAndServe(":8080", r)
+}
+
+const content = ``
+
+func WorkerTest() {
+	t := time.NewTicker(1 * time.Second)
+	var b byte = 0
+	for {
+		select {
+		case <-t.C:
+			err := os.WriteFile("/app/static/_index.md", []byte(fmt.Sprintf(content, b)), 0644)
+			if err != nil {
+				log.Println(err)
+			}
+			b++
+		}
+	}
+}
